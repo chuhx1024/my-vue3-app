5b9c81709735583aea7263dcbec45b6ae76231fc
chuhx <chuhx1024@126.com>
2023-06-22 17:14:11 +0800

    fix: 忘记安装 postcss-pxtorem

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vitePluginEslint from "vite-plugin-eslint"
 
 const postCssPxToRem = require("postcss-pxtorem")
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
